cc09e4e1433dde9a13ac4da304c7fc6728154380
Bruce Park <bruce@binarywebpark.com>
2014-05-23 23:03:38 -0700

    finish upload photo

--- nodebeginner/index-handler.js
+++ nodebeginner/index-handler.js
@@ -1,11 +1,12 @@
-var server = require("./module-server-route-handler-asynch-listener");
-var router = require("./router-handler-asynch-listener");
-var requestHandlers = require("./requestHandlersAsynchListener");
+var server = require("./module-server-route-handler-asynch-listener-show");
+var router = require("./router-handler-asynch-listener-show");
+var requestHandlers = require("./requestHandlersAsynchListenerShow");
 
 var handle = {}
 
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
